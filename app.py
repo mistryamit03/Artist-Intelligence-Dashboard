d282bd2868e25f49f0b67ca6ce421727936ef9a8
mistryamit03 <mistryamit03@gmail.com>
2026-05-13 23:35:52 +0200

    Initial project structure

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title= "Artist Intelligence Dashbaord",
+    page_icon= "🎧",
+    layout="wide"
+)
+
+
+st.title("Artist Intelligence Dashbaord")
+st.write("Project setup complete. Dashboard build starts in Phase 1.")
